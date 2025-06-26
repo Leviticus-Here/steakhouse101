61dd4a5f02b4514a08a5342401629c50d3819291
Leviticus Harrison <20240823@mywhitecliffe.com>
2025-06-26 14:00:15 +1200

    Fix anchor href to meet accessibility lint rules

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import './Navbar.css';
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
     <div className="container">
-      <a className="navbar-brand" href="#">Steakhouse 101</a>
+      <button className="nav-link" onClick={() => scrollToSection('home')}>Steakhouse 101</button>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
         <span className="navbar-toggler-icon"></span>
       </button>
