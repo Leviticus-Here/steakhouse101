2691067d2a9f680a62a19e5944e3a6df90411f38
Leviticus Harrison <20240823@mywhitecliffe.com>
2025-06-27 14:56:02 +1200

    Message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contact from './components/Contact';
+import Contact from './components/Contact.js';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import MenuGallery from './components/MenuGallery';
