c4ce32013879683ff74bfd6b25417f0355758ff9
Leviticus Harrison <20240823@mywhitecliffe.com>
2025-06-27 14:51:01 +1200

    Message

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './components/Contact';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import MenuGallery from './components/MenuGallery';
@@ -25,6 +27,13 @@ function App() {
       <Footer />
       {/* other components */}
       <ScrollToTop />
+      <Router>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/contact" element={<Contact />} />
+        {/* Add more pages here */}
+      </Routes>
+    </Router>
     </>
   );
 }
